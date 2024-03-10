5f48c63f0e92937af9f293a1456d79e764661463
Francisco Pereira <fchamicapereira@gmail.com>
2024-03-10 17:40:36 -0400

    Added more control on dump_flows script

--- dump_flows.sh
+++ dump_flows.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+flows=$1
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ./dump_flows.sh <number of flows>"
+    exit 1
+fi
+
 ./build.sh
 echo quit | sudo ./Debug/bin/pktgen \
     -m 8192 \
@@ -8,7 +17,7 @@ echo quit | sudo ./Debug/bin/pktgen \
     --vdev "net_tap0,iface=test_rx" \
     --vdev "net_tap1,iface=test_tx" \
     -- \
-    --total-flows 2 \
+    --total-flows $flows \
     --pkt-size 64 \
     --tx 1 \
     --rx 0 \
